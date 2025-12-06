8f3fe73c840784244f7ef0d6e0c91d1e762cf4e9
Omico <me@omico.icu>
2025-12-07 07:21:37 +0800

    kuaishou-kim 5.0.6 (#3)

--- Casks/kuaishou-kim.rb
+++ Casks/kuaishou-kim.rb
@@ -1,9 +1,9 @@
 cask "kuaishou-kim" do
   arch arm: "-arm"
 
-  version "5.0.5,59640"
-  sha256 arm:   "9b15259e79e8ba6a8ab7d9c22d9e6196b1838b18b2df8abd425a9118fc3379f4",
-         intel: "cf734aa9bbf1f44f820e00f9f12a476594ebc45081bf7228902481886c17be37"
+  version "5.0.6,59724"
+  sha256 arm:   "b955b43d39f83084a927b7a1d39d18e8ef7e25239c17a513774edb0ee375a7ca",
+         intel: "228d344c7bb54e56e04554f53f6b281c54f588ec89701d9256be17be910ebe58"
 
   url "https://kim.static.yximgs.com/udata/pkg/kim-install/Kim-#{version.csv.first}-#{version.csv.second}#{arch}.dmg",
       verified: "kim.static.yximgs.com/udata/pkg/kim-install/"
